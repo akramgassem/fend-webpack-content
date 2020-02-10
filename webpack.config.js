6ee586ec31db0396cb4861ef885e7e0c5696024d
akramgassem <akramgassem@gmail.com>
2020-02-10 19:41:24 +0100

    install eslint eslint-loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const json = require('json-loader!./file.json');
 const webpack = require('webpack');
 
 module.exports = {
@@ -6,6 +7,17 @@ module.exports = {
     devtool: 'source-map',
     module: {
         rules: [
+            {
+                enforce: 'pre',
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'eslint-loader',
+                options: {
+                    cache: true,
+                    emitWarning: true,
+                    fix: true
+                  }
+              },
             {
                 test: '/\.js$/',
                 exclude: /node_modules/,
