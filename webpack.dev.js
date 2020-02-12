c118a7676a7d17bc2ea6e72af54ea4cfa786907d
akramgassem <akramgassem@gmail.com>
2020-02-12 19:56:09 +0100

    add tasks

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,24 +1,48 @@
-const path = require('path')
-const webpack = require('webpack')
-const HtmlWebPackPlugin = require("html-webpack-plugin")
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer')
+	.BundleAnalyzerPlugin;
 
 module.exports = {
-    entry: './src/client/index.js',
-    mode: 'development',
-    devtool: 'source-map',
-    module: {
-        rules: [
-            {
-                test: '/\.js$/',
-                exclude: /node_modules/,
-                loader: "babel-loader"
-            }
-        ]
-    },
-    plugins: [
-        new HtmlWebPackPlugin({
-            template: "./src/client/views/index.html",
-            filename: "./index.html",
-        })
-    ]
-}
+	entry: './src/client/index.js',
+	output: {
+		filename: '[name].bundle.js',
+		path: path.resolve(__dirname, 'dist')
+	},
+	mode: 'development',
+	devtool: 'inline-source-map',
+	devServer: {
+		contentBase: './dist',
+		hot: true
+	},
+	module: {
+		rules: [
+			{
+				test: /\.css$/i,
+				use: ['style-loader', 'css-loader']
+			},
+			{
+				test: /\.(png|jpe?g|gif|svg|eot|ttf|woff|woff2)$/i,
+				loader: 'url-loader',
+				options: {
+					limit: 8192
+				}
+			},
+			{
+				test: '/.js$/',
+				exclude: /node_modules/,
+				loader: 'babel-loader'
+			}
+		]
+	},
+	plugins: [
+		new CleanWebpackPlugin(),
+		new HtmlWebPackPlugin({
+			template: './src/client/views/index.html',
+			filename: './index.html'
+		}),
+	    new BundleAnalyzerPlugin()
+	]
+};
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,23 +1,44 @@
-const path = require('path')
-const webpack = require('webpack')
-const HtmlWebPackPlugin = require("html-webpack-plugin")
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-    entry: './src/client/index.js',
-    mode: 'production',
-    module: {
-        rules: [
-            {
-                test: '/\.js$/',
-                exclude: /node_modules/,
-                loader: "babel-loader"
-            }
-        ]
-    },
-    plugins: [
-        new HtmlWebPackPlugin({
-            template: "./src/client/views/index.html",
-            filename: "./index.html",
-        })
-    ]
-}
+	entry: './src/client/index.js',
+	output: {
+		filename: '[name].bundle.js',
+		path: path.resolve(__dirname, 'dist')
+	},
+	mode: 'production',
+	devServer: {
+		contentBase: './dist',
+		hot: true
+	},
+	module: {
+		rules: [
+			{
+				test: /\.css$/i,
+				use: ['style-loader', 'css-loader']
+			},
+			{
+				test: /\.(png|jpe?g|gif|svg|eot|ttf|woff|woff2)$/i,
+				loader: 'url-loader',
+				options: {
+					limit: 8192
+				}
+			},
+			{
+				test: '/.js$/',
+				exclude: /node_modules/,
+				loader: 'babel-loader'
+			}
+		]
+	},
+	plugins: [
+		new CleanWebpackPlugin(),
+		new HtmlWebPackPlugin({
+			template: './src/client/views/index.html',
+			filename: './index.html'
+		})
+	]
+};
