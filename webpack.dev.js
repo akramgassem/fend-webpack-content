79bedf21377a1915d35c21cf5889c8af6e3ceec1
akramgassem <akramgassem@gmail.com>
2020-02-15 22:43:31 +0100

    add config prod

--- webpack.dev.js
+++ webpack.dev.js
@@ -2,41 +2,50 @@ const path = require("path");
 const webpack = require("webpack");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+// const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+// const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
-  entry: "./src/client/index.js",
-  mode: "development",
-  devtool: "source-map",
-  stats: "normal",
-  output: {
-    libraryTarget: "var",
-    library: "Client"
-  },
-  module: {
-    rules: [
-      {
-        test: /\.scss$/,
-        use: ["style-loader", "css-loader", "postcss-loader", "sass-loader"]
-      },
-      {
-        test: "/.js$/",
-        exclude: /node_modules/,
-        loader: "babel-loader"
-      }
-    ]
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-        template: "./src/client//views/index.html"
-      }),
-    new CleanWebpackPlugin({
-      // Simulate the removal of files
-      dry: true,
-      // Write Logs to Console
-      verbose: true,
-      // Automatically remove all unused webpack assets on rebuild
-      cleanStaleWebpackAssets: true,
-      protectWebpackAssets: false
-    })
-  ]
+	entry: './src/client/index.js',
+	mode: 'development',
+	devtool: 'source-map',
+	stats: 'normal',
+	output: {
+		libraryTarget: 'var',
+		library: 'Client'
+	},
+	module: {
+		rules: [
+			{
+				test: /\.scss$/,
+				use: [
+					MiniCssExtractPlugin.loader,
+					'css-loader',
+					'postcss-loader',
+					'sass-loader'
+				]
+			},
+			{
+				test: '/.js$/',
+				exclude: /node_modules/,
+				loader: 'babel-loader'
+			}
+		]
+	},
+	plugins: [
+		new MiniCssExtractPlugin({ filename: '[name].css' }),
+		new HtmlWebpackPlugin({
+			template: './src/client//views/index.html'
+		}),
+		new CleanWebpackPlugin({
+			// Simulate the removal of files
+			dry: true,
+			// Write Logs to Console
+			verbose: true,
+			// Automatically remove all unused webpack assets on rebuild
+			cleanStaleWebpackAssets: true,
+			protectWebpackAssets: false
+		})
+	]
 };
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,23 +1,53 @@
-const path = require('path')
-const webpack = require('webpack')
-const HtmlWebPackPlugin = require("html-webpack-plugin")
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
-    entry: './src/client/index.js',
-    mode: 'production',
-    module: {
-        rules: [
-            {
-                test: '/\.js$/',
-                exclude: /node_modules/,
-                loader: "babel-loader"
-            }
-        ]
-    },
-    plugins: [
-        new HtmlWebPackPlugin({
-            template: "./src/client/views/index.html",
-            filename: "./index.html",
-        })
-    ]
-}
+	entry: './src/client/index.js',
+	mode: 'production',
+	stats: 'verbose',
+	optimization: {
+		minimizer: [new TerserPlugin({}), new OptimizeCSSAssetsPlugin({})]
+	},
+	output: {
+		libraryTarget: 'var',
+		library: 'Client'
+	},
+	module: {
+		rules: [
+			{
+				test: /\.scss$/,
+				use: [
+					MiniCssExtractPlugin.loader,
+					'css-loader',
+					'postcss-loader',
+					'sass-loader'
+				]
+			},
+			{
+				test: '/.js$/',
+				exclude: /node_modules/,
+				loader: 'babel-loader'
+			}
+		]
+	},
+	plugins: [
+		new MiniCssExtractPlugin({ filename: '[name].css' }),
+		new HtmlWebpackPlugin({
+			template: './src/client//views/index.html'
+		}),
+		new CleanWebpackPlugin({
+			// Simulate the removal of files
+			dry: true,
+			// Write Logs to Console
+			verbose: true,
+			// Automatically remove all unused webpack assets on rebuild
+			cleanStaleWebpackAssets: true,
+			protectWebpackAssets: false
+		})
+	]
+};
