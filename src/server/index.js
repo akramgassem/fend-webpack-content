c000168d6e120ef1e783de5e8ba18500dbb05a2c
akramgassem <akramgassem@gmail.com>
2020-02-15 07:22:58 +0100

    fix server fix import functions fix scss loaders fix jshint install dependencies

--- src/server/index.js
+++ src/server/index.js
@@ -3,20 +3,25 @@ const express = require('express')
 const mockAPIResponse = require('./mockAPI.js')
 
 const app = express()
+const cors = require('cors')
 
 app.use(express.static('dist'))
 
+app.use(cors())
+
 console.log(__dirname)
 
-app.get('/', function (req, res) {
+app.get('/', (req, res) => {
     res.sendFile('dist/index.html')
 })
 
+const PORT = 8081;
+
 // designates what port the app will listen to for incoming requests
-app.listen(8080, function () {
-    console.log('Example app listening on port 8080!')
+app.listen(PORT,  () => {
+    console.log('Example app listening on port 8081!')
 })
 
-app.get('/test', function (req, res) {
+app.get('/test', (req, res) => {
     res.send(mockAPIResponse)
 })
